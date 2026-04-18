25456c8ca8a9807ebb33e90978733276babf4290
isHardCoded <ishardcoded@gmail.com>
2026-04-18 15:07:09 +0300

    feat: сверстали страницу корзины

--- pizza/pages/cart/cart.js
+++ pizza/pages/cart/cart.js
@@ -1,10 +1,10 @@
-const pizzas = localStorage.getItem("pizzas")
-const parsedPizzas = JSON.parse(pizzas)
+// const pizzas = localStorage.getItem("pizzas")
+// const parsedPizzas = JSON.parse(pizzas)
 
-parsedPizzas.forEach(pizza => {
-  const div = document.createElement('div')
-  div.innerHTML = `
-    <h2>${pizza.name}</h2>
-  `
-  document.querySelector('.container').appendChild(div)
-})
+// parsedPizzas.forEach(pizza => {
+//   const div = document.createElement('div')
+//   div.innerHTML = `
+//     <h2>${pizza.name}</h2>
+//   `
+//   document.querySelector('.container').appendChild(div)
+// })
